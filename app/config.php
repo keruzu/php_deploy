01033aa8a245cab5314d8df5f01764bac88df9d5
Kearney, Kells <keruzu@gmail.com>
2024-11-05 17:42:05 -0500

    Refactor out creds to external file

--- app/config.php
+++ app/config.php
@@ -1,8 +1,31 @@
 <?php
-$servername = "db";
-$username = "app_user";
-$password = "s3kr3ts3kr3ts3kr3t";
-$dbname = "example_db";
+
+function readKeyValueFile($filename) {
+    $keyValuePairs = [];
+
+    if (file_exists($filename)) {
+        $lines = file($filename, FILE_IGNORE_NEW_LINES);
+
+        foreach ($lines as $line) {
+            $parts = explode('=', $line, 2);
+            if (count($parts) == 2) {
+                $key = trim($parts[0]);
+                $value = trim($parts[1]);
+                $keyValuePairs[$key] = $value;
+            }
+        }
+    }
+
+    return $keyValuePairs;
+}
+
+$filename = '/var/www/config.txt';
+$config = readKeyValueFile($filename);
+
+$servername = $config["hostname"];
+$username = $config["username"];
+$password = $config["password"];
+$dbname = $config["dbname"];
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
